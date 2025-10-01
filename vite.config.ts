b6860966ee28d70564e2275863b653fb743482b6
alescire <alescire94@gmail.com>
2025-10-01 19:40:36 +0200

    fix deploy filepath

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 // vite.config.ts
 import { defineConfig } from 'vite'
 
-export default defineConfig(({ command }) => ({
-    // dev -> '/', build -> '/marriage_invite/' (repo name on GitHub Pages)
-    base: command === 'build' ? '/marriage_invite/' : '/',
-    build: { outDir: 'dist' }
-}))
+export default defineConfig({
+    base: '/marriage_invite/',
+    build: {
+        outDir: 'dist'
+    }
+})
